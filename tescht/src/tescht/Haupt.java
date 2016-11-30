45ceac03069d0154dbda1b0965ce41d325596175
Adrian <Adrian@Adrian-PC1>
2016-11-30 23:44:41 +0100

    Alles ok

--- tescht/src/tescht/Haupt.java
+++ tescht/src/tescht/Haupt.java
@@ -6,6 +6,7 @@ public class Haupt {
 		System.out.println("Hallo Andre!/n geht doch alles");
 		System.out.println("oder etwa nicht?");
 		//Mensch andre das muss doch gehen
+		System.out.println("Alles rogger");
 	}
 
 }
