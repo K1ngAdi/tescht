7952cb55a096996b973a0867ec47bd18fc384e44
Adrian <Adrian@Adrian-PC1>
2016-11-30 22:56:24 +0100

    schau mal nach

--- tescht/src/tescht/Haupt.java
+++ tescht/src/tescht/Haupt.java
@@ -5,7 +5,7 @@ public class Haupt {
 	public static void main(String[] args) {
 		System.out.println("Hallo Andre!/n geht doch alles");
 		System.out.println("oder etwa nicht?");
-
+		//Mensch andre das muss doch gehen
 	}
 
 }
