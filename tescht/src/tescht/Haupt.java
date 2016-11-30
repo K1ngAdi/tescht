b05b7dfa21f11616c72bcab4d38131c35768d6fd
andre-jar <andrednagel@web.de>
2016-11-30 22:39:21 +0100

    Update
    Hallo Test Test

--- tescht/src/tescht/Haupt.java
+++ tescht/src/tescht/Haupt.java
@@ -5,7 +5,7 @@ public class Haupt {
 	public static void main(String[] args) {
 		System.out.println("Hallo Andre!/n geht doch alles");
 		System.out.println("oder etwa nicht?");
-
+                System.out.println("Hallo ich bin eine neue Zeile verfasst mit Firefox 50.0.1");
 	}
 
 }
