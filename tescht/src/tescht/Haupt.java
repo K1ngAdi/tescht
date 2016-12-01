91c73d52dab3c20729a47ffc3cc58f7be0583b24
K1ngAdi <adrifri@gmx.de>
2016-12-01 22:35:28 +0100

    asdfgehe

--- tescht/src/tescht/Haupt.java
+++ tescht/src/tescht/Haupt.java
@@ -7,7 +7,7 @@ public class Haupt {
 		System.out.println("oder etwa nicht?");
 		//Mensch andre das muss doch gehen
 		System.out.println("Alles rogger");
-		System.out.println("in Tokio");//asdf
+		System.out.println("in Tokio");//asdfasaasad
 	}
 
 }
