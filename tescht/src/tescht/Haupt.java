5b986fd05e81cf9e4997682bc87922a77aff74a9
andre-jar <andrednagel@web.de>
2016-12-01 13:21:35 +0100

    Update Haupt.java
    Hi

--- tescht/src/tescht/Haupt.java
+++ tescht/src/tescht/Haupt.java
@@ -7,6 +7,7 @@ public class Haupt {
 		System.out.println("oder etwa nicht?");
 		//Mensch andre das muss doch gehen
 		System.out.println("Alles rogger");
+		System.out.println("In Cambodscha");
 	}
 
 }
